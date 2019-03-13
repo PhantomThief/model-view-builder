a0c653cae50d526d641e9a4041742584397424c2
w.vela <wangtianzhou@kuaishou.com>
2019-03-13 17:24:03 +0800

    Revert "[optimize] add log for empty ids building."
    This reverts commit 4589dc4a932de0bf5c88ca045f43aebe0bc794d3.

--- src/main/java/com/github/phantomthief/model/builder/impl/SimpleModelBuilder.java
+++ src/main/java/com/github/phantomthief/model/builder/impl/SimpleModelBuilder.java
@@ -108,10 +108,6 @@ public class SimpleModelBuilder<B extends BuildContext> implements ModelBuilder<
                     Set<Object> needToBuildIds = filterIdSetOnBuild(ids, buildContext, valuesMap,
                             valueNamespace);
                     Map<Object, Object> values = valueBuilder.apply(buildContext, needToBuildIds);
-                    if (needToBuildIds.isEmpty() && values != null && !values.isEmpty()) {
-                        logger.warn("found illegal build logic for namespace:{}=>{}", idNamespace,
-                                valueNamespace);
-                    }
                     if (values != null) {
                         valuesMap.merge(valueNamespace, values, MergeUtils::merge);
                     }
